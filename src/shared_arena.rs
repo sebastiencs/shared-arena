2bde4965126530346303abb92d27aea1584e9d56
Sebastien Chapuis <sebastien@chapu.is>
2020-08-02 16:12:09 +0800

    Make to_free a Box earlier

--- src/shared_arena.rs
+++ src/shared_arena.rs
@@ -105,12 +105,9 @@ impl<T: Sized> SharedArena<T> {
                 let to_free = self.to_free.swap(std::ptr::null_mut(), AcqRel);
 
                 if let Some(to_free) = unsafe { to_free.as_mut() } {
-                    // let to_free = unsafe { Box::from_raw(to_free) };
+                    let to_free = unsafe { Box::from_raw(to_free) };
                     for page in &*to_free {
                         drop_page(page.as_ptr());
-                    }
-                    unsafe {
-                        Box::from_raw(to_free);
                     }
                 }
             }
@@ -203,8 +200,7 @@ impl<T: Sized> SharedArena<T> {
         if let Some(to_free) = unsafe {
             self.to_free.swap(std::ptr::null_mut(), AcqRel).as_mut()
         } {
-            // let mut to_free = to_free.into_vec();
-            // let mut to_free = unsafe { Box::from_raw(to_free) };
+            let mut to_free = unsafe { Box::from_raw(to_free) };
 
             let npages = self.npages.load(Relaxed).max(1);
             let truncate_at = to_free.len().saturating_sub(npages);
@@ -212,20 +208,13 @@ impl<T: Sized> SharedArena<T> {
 
             let (first, last) = PageSharedArena::make_list_from_slice(&to_reinsert);
             self.put_pages_in_lists(to_reinsert.len(), first, last);
-            eprintln!("TO_INSERT={}", to_reinsert.len());
 
             if truncate_at != 0 {
-                eprintln!("TRUNCATE_AT={} BEFORE={}", truncate_at, to_free.len());
                 to_free.truncate(truncate_at);
-                eprintln!("AFTER={}", to_free.len());
-                let old_to_free = self.to_free.swap(to_free, Release);
+                let old_to_free = self.to_free.swap(Box::into_raw(to_free), Release);
                 assert!(old_to_free.is_null());
                 self.to_free_delay.store(0, Relaxed);
             } else {
-                unsafe {
-                    Box::from_raw(to_free);
-                    // std::ptr::drop_in_place(to_free);
-                }
                 self.to_free_delay.store(DELAY_DROP_SHRINK, Relaxed);
             }
         }
@@ -625,8 +614,8 @@ impl<T: Sized> SharedArena<T> {
                 assert!(old.is_null());
             } else {
                 let ptr = Box::new(to_drop);
-                let old_to_free = self.to_free.swap(Box::into_raw(ptr), AcqRel);
-                assert!(old_to_free.is_null());
+                let old = self.to_free.swap(Box::into_raw(ptr), AcqRel);
+                assert!(old.is_null());
             }
             self.to_free_delay.store(0, Release);
         }
@@ -731,14 +720,9 @@ impl<T: Sized> SharedArena<T> {
 impl<T> Drop for SharedArena<T> {
     fn drop(&mut self) {
         if let Some(to_free) = unsafe { self.to_free.load(Relaxed).as_mut() } {
-            // let to_free = unsafe { Box::from_raw(to_free) };
+            let to_free = unsafe { Box::from_raw(to_free) };
             for page in &*to_free {
                 drop_page(page.as_ptr());
-            }
-
-            unsafe {
-                Box::from_raw(to_free);
-            //     std::ptr::drop_in_place(to_free);
             }
         }
 
